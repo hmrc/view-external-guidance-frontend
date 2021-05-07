f7e44e29a5818a4e168db72836d0ab0de872463c
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2021-05-07 12:25:26 +0100

    EG-1407, review update

--- app/controllers/SessionTimeoutPageController.scala
+++ app/controllers/SessionTimeoutPageController.scala
@@ -54,7 +54,7 @@ class SessionTimeoutPageController @Inject()(appConfig: AppConfig,
             case Right(processContext) if processCode != processContext.process.meta.processCode =>
               logger.error(s"Unexpected process code encountered when removing session after timeout warning. " +
                 s"Expected code $processCode; actual code ${processContext.process.meta.processCode}")
-              // Session not as expected, user muyse be running another piece of guidance, signal answers deleted
+              // Session not as expected, user must be running another piece of guidance, signal answers deleted
               Future.successful(Ok(createDeleteYourAnswersResponse(messages("session.timeout.header.title"), processCode)).withNewSession)
             case Right(processContext) =>
               Future.successful(Ok(createDeleteYourAnswersResponse(processContext.process.title.value(messages.lang), processCode)).withNewSession)
