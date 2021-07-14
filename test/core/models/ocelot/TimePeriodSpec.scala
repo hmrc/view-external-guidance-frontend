016c4c28a55015f2c5fc52fe7f6af81e318ea07e
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2021-07-14 11:15:34 +0100

    DL-5842, TimePeriod, regex and tests

---
+++ test/core/models/ocelot/TimePeriodSpec.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package core.models.ocelot
+
+import base.BaseSpec
+
+class TimePeriodSpec extends BaseSpec {
+
+  "A TimePeriod instance" must {
+    "construct from a number of days" in {
+      asTimePeriod("23455day") shouldBe Some(TimePeriod(23455, Day))
+    }
+    "construct from a number of weeks" in {
+      asTimePeriod("23455day") shouldBe Some(TimePeriod(23455, Day))
+    }
+    "construct from a number of months" in {
+      asTimePeriod("23455day") shouldBe Some(TimePeriod(23455, Day))
+    }
+    "construct from a number of years" in {
+      asTimePeriod("23455day") shouldBe Some(TimePeriod(23455, Day))
+    }
+
+    "not construct when the count digit length is greater than the digit length of Int.MaxValue" in {
+      asTimePeriod("12345678901day") shouldBe None
+    }
+  }
+}
