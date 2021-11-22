acd6b26b47ad280c033fc31a7f9fb2c3a80f21b7
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2021-11-22 13:46:17 +0000

    DL-6667, stanza count > 100 => internal server errror

--- app/controllers/GuidanceController.scala
+++ app/controllers/GuidanceController.scala
@@ -24,6 +24,7 @@ import play.api.mvc._
 import play.api.data.Form
 import services.{ErrorStrategy, GuidanceService, ValueTypeError}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import core.models.RequestOutcome
 import core.models.errors._
 import core.models.ocelot.SecuredProcess
 import models.{PageContext, PageEvaluationContext, POST}
@@ -103,16 +104,16 @@ class GuidanceController @Inject() (
           val inputName: String = formInputName(path)
           bindFormData(input, inputName) match {
             case Left((formWithErrors: Form[_], errorStrategy: ErrorStrategy)) =>
-                Future.successful(BadRequest(createInputView(service.getPageContext(ctx, errorStrategy), inputName, formWithErrors)))
+                Future.successful(BadRequest(createErrorInputView(service.getPageContext(ctx, errorStrategy), inputName, formWithErrors)))
             case Right((form: Form[_], submittedAnswer: SubmittedAnswer)) =>
               service.validateUserResponse(ctx, submittedAnswer.text).fold{
                 // Answer didn't pass page DataInput stanza validation
-                Future.successful(BadRequest(createInputView(service.getPageContext(ctx, ValueTypeError), inputName, form)))
+                Future.successful(BadRequest(createErrorInputView(service.getPageContext(ctx, ValueTypeError), inputName, form)))
               }{ answer =>
                 service.submitPage(ctx, s"/$path", answer, submittedAnswer.text).map {
                   case Right((None, labels)) =>      // No valid next page id indicates page should be re-displayed
                     logger.info(s"Post submit page evaluation indicates guidance detected input error")
-                    BadRequest(createInputView(service.getPageContext(ctx.copy(labels = labels)), inputName, form))
+                    BadRequest(createErrorInputView(service.getPageContext(ctx.copy(labels = labels)), inputName, form))
                   case Right((Some(stanzaId), _)) => // Some(stanzaId) here indicates a redirect to the page with id "stanzaId"
                     val url = ctx.pageMapById(stanzaId).url
                     logger.info(s"Post submit page evaluation indicates next page at stanzaId: $stanzaId => $url")
@@ -163,10 +164,10 @@ class GuidanceController @Inject() (
         logger.warn(s"Request for PageContext at /$path returned BadRequest during form submission, returning BadRequest")
         BadRequest(errorHandler.badRequestTemplateWithProcessCode(Some(processCode)))
       case ExpectationFailedError =>
-        logger.warn(s"ExpectationFailed error on submitPage. Redirecting to start of processCode $processCode at ${appConfig.baseUrl}/$processCode")
+        logger.warn(s"ExpectationFailed error on submitPage. Redirect to ${appConfig.baseUrl}/$processCode")
         Redirect(s"${appConfig.baseUrl}/$processCode")
       case SessionNotFoundError =>
-        logger.warn(s"Request for page at /$path returned SessionNotFound. Redirecting to start of processCode $processCode at ${appConfig.baseUrl}/$processCode")
+        logger.warn(s"Request for page at /$path returned SessionNotFound. Redirect to ${appConfig.baseUrl}/$processCode")
         Redirect(s"${appConfig.baseUrl}/$processCode")
       case err =>
         logger.error(s"Request for PageContext at /$path returned $err during form submission, returning InternalServerError")
@@ -185,7 +186,7 @@ class GuidanceController @Inject() (
           Redirect(s"${appConfig.baseUrl}/${processCode}${idToUrlMap(x)}")
       }
       case Left(err) =>
-        logger.warn(s"Failed ($err) to retrieve current session on ForbiddenError, redirecting after ForbiddenError to beginning of process")
+        logger.warn(s"Failed ($err) to retrieve current session, redirecting after ForbiddenError to beginning of process")
         Redirect(s"${appConfig.baseUrl}/$processCode")
     }
 
@@ -195,24 +196,30 @@ class GuidanceController @Inject() (
         logger.warn(s"Request for PageContext at /$path returned AuthenticationError, redirecting to process passphrase page")
         Redirect(routes.GuidanceController.getPage(processCode, SecuredProcess.SecuredProcessStartUrl, None))
       case SessionNotFoundError =>
-        logger.warn(s"Request for page at /$path returned SessionNotFound. Redirecting to start of processCode $processCode at ${appConfig.baseUrl}/$processCode")
+        logger.warn(s"Request for page at /$path returned SessionNotFound. Redirect to ${appConfig.baseUrl}/$processCode")
         Redirect(s"${appConfig.baseUrl}/$processCode")
       case NotFoundError =>
         logger.warn(s"Request for PageContext at /$path returned NotFound, returning NotFound")
         NotFound(errorHandler.notFoundTemplateWithProcessCode(Some(processCode)))
       case ExpectationFailedError =>
-        logger.warn(s"ExpectationFailed error on getPage. Redirecting to start of processCode $processCode at ${appConfig.baseUrl}/$processCode")
+        logger.warn(s"ExpectationFailed error on getPage. Redirecting to ${appConfig.baseUrl}/$processCode")
         Redirect(s"${appConfig.baseUrl}/$processCode")
       case err =>
         logger.error(s"Request for PageContext at /$path returned $err, returning InternalServerError")
         InternalServerError(errorHandler.internalServerErrorTemplate)
     }
 
-  private def createInputView(ctx: PageContext, inputName: String, form: Form[_])(implicit request: Request[_], messages: Messages): Html =
-    ctx.page match {
-      case page: FormPage => formView(page, ctx, inputName, form)
-      case _ => errorHandler.badRequestTemplateWithProcessCode(Some(ctx.processCode))
+  private def createErrorInputView(ctxOutcome: RequestOutcome[PageContext], inputName: String, form: Form[_])
+                                  (implicit request: Request[_], messages: Messages): Html =
+    ctxOutcome match {
+      case Left(err) => errorHandler.internalServerErrorTemplateWithProcessCode(None)
+      case Right(ctx) =>
+        ctx.page match {
+          case page: FormPage => formView(page, ctx, inputName, form)
+          case _ => errorHandler.badRequestTemplateWithProcessCode(Some(ctx.processCode))
+        }
     }
+
 
   private def formInputName(path: String): String = path.reverse.takeWhile(_ != '/').reverse
   private def previousPageQueryString(url: String, backLink: Option[String]): Option[String] = backLink.collect{case bl if bl == url => "1"}
--- app/services/PageRenderer.scala
+++ app/services/PageRenderer.scala
@@ -19,71 +19,80 @@ package services
 import javax.inject.{Inject, Singleton}
 import scala.annotation.tailrec
 import config.AppConfig
+import core.models.RequestOutcome
+import core.models.errors.NonTerminatingPageError
 import core.models.ocelot.stanzas.{PageStanza, EndStanza, VisualStanza, Stanza, Evaluate, DataInput}
 import core.models.ocelot.{Page, Labels, Process}
 
 @Singleton
 class PageRenderer @Inject() (appConfig: AppConfig) {
-  val PageStanzaLimit: Int = appConfig.pageStanzaLimit
+  val PageStanzaLimit: Int = appConfig.pageStanzaLimit // Limit stanzas within page to catch non-terminating loops in guidance
 
-  def renderPage(page: Page, labels: Labels): (Seq[VisualStanza], Labels, Option[DataInput]) = {
+  def renderPage(page: Page, labels: Labels): RequestOutcome[(Seq[VisualStanza], Labels, Option[DataInput])] = {
     implicit val stanzaMap: Map[String, Stanza] = page.keyedStanzas.map(ks => (ks.key, ks.stanza)).toMap ++ labels.continuationPool
-    val (visualStanzas, newLabels, _, _, optionalInput) = evaluateStanzas(stanzaMap(page.id).next.head, labels)
-    (visualStanzas, newLabels, optionalInput)
+    evaluateStanzas(stanzaMap(page.id).next.head, labels) match {
+      case Right((visualStanzas, newLabels, _, _, optionalInput)) => Right((visualStanzas, newLabels, optionalInput))
+      case Left(err) => Left(err)
+    }
   }
 
-  def renderPagePostSubmit(page: Page, labels: Labels, answer: String): (Option[String], Labels) = {
+  def renderPagePostSubmit(page: Page, labels: Labels, answer: String): RequestOutcome[(Option[String], Labels)] = {
 
     @tailrec
-    def evaluatePostInputStanzas(next: String, labels: Labels, seen: Seq[String])(implicit stanzaMap: Map[String, Stanza]): (Option[String], Labels) =
-      if (seen.contains(next)) {(None, labels)}   // next indicates any seen id
+    def evaluatePostInputStanzas(next: String, labels: Labels, seen: Seq[String], stanzaCount: Int = 0)
+                                (implicit stanzaMap: Map[String, Stanza]): RequestOutcome[(Option[String], Labels)] =
+      if (seen.contains(next)) {Right((None, labels))}   // next indicates any seen id
       else {stanzaMap.get(next) match {
-        case None => (Some(next), labels)
-        case Some(s) => s match {
-          case _: PageStanza => (Some(next), labels)
+        case None => Right((Some(next), labels))
+        case Some(s) if stanzaCount < PageStanzaLimit => s match {
+          case _: PageStanza => Right((Some(next), labels))
           case EndStanza => labels.nextFlow match {
-              case Some((nxt, updatedLabels)) => evaluatePostInputStanzas(nxt, updatedLabels, seen)
+              case Some((nxt, updatedLabels)) => evaluatePostInputStanzas(nxt, updatedLabels, seen, stanzaCount+1)
               // Encountering an EndStanza following input suggests incomplete guidance, i.e. a form page which accepts input and stops
               // Therefore handle as if guidance indicated a Value error and cause current form page to be re-displayed. Logically also
               // an EndStanza indicates there is no Next!
-              case None => (None, labels)
+              case None => Right((None, labels))
             }
           case s: Stanza with Evaluate =>
             val (next, updatedLabels) = s.eval(labels)
-            evaluatePostInputStanzas(next, updatedLabels, seen)
+            evaluatePostInputStanzas(next, updatedLabels, seen, stanzaCount+1)
         }
+      case Some(s) => Left(NonTerminatingPageError)
       }}
 
     implicit val stanzaMap: Map[String, Stanza] = page.keyedStanzas.map(ks => (ks.key, ks.stanza)).toMap ++ labels.continuationPool
-    val (_, newLabels, seen, nextPageId, optionalInput) = evaluateStanzas(stanzaMap(page.id).next.head, labels)
-
-    optionalInput.fold[(Option[String], Labels)]((Some(nextPageId), newLabels)){dataInputStanza =>
-      dataInputStanza.eval(answer, page, newLabels) match {
-        case (Some(Process.EndStanzaId), updatedLabels) => updatedLabels.nextFlow match {
-            case Some((next, updatedLabels)) => evaluatePostInputStanzas(next, updatedLabels, seen)
-            // Encountering an EndStanza following input suggests incomplete guidance, i.e. a form page which accepts input and stops
-            // Therefore handle as if guidance indicated a Value error and cause current form page to be re-displayed. Logically also
-            // an EndStanza indicates there is no Next!
-            case None => (None, updatedLabels)
-          }
-        case (Some(next), updatedLabels) => evaluatePostInputStanzas(next, updatedLabels, seen)
-        case (None, updatedLabels) => (None, updatedLabels)
-       }
+    evaluateStanzas(stanzaMap(page.id).next.head, labels) match {
+      case Right((_, newLabels, seen, nextPageId, optionalInput)) =>
+        optionalInput.fold[RequestOutcome[(Option[String], Labels)]](Right((Some(nextPageId), newLabels))){dataInputStanza =>
+          dataInputStanza.eval(answer, page, newLabels) match {
+            case (Some(Process.EndStanzaId), updatedLabels) => updatedLabels.nextFlow match {
+                case Some((next, updatedLabels)) => evaluatePostInputStanzas(next, updatedLabels, seen)
+                // Encountering an EndStanza following input suggests incomplete guidance, i.e. a form page which accepts input and stops
+                // Therefore handle as if guidance indicated a Value error and cause current form page to be re-displayed. Logically also
+                // an EndStanza indicates there is no Next!
+                case None => Right((None, updatedLabels))
+              }
+            case (Some(next), updatedLabels) => evaluatePostInputStanzas(next, updatedLabels, seen)
+            case (None, updatedLabels) => Right((None, updatedLabels))
+           }
+        }
+      case Left(err) => Left(err)
     }
   }
 
   @tailrec
-   private def evaluateStanzas(stanzaId: String, labels: Labels, visualStanzas: Seq[VisualStanza] = Nil, seen: Seq[String] = Nil)
-                              (implicit stanzaMap: Map[String, Stanza]): (Seq[VisualStanza], Labels, Seq[String], String, Option[DataInput]) =
+   private def evaluateStanzas(stanzaId: String, labels: Labels, visualStanzas: Seq[VisualStanza] = Nil, seen: Seq[String] = Nil, stanzaCount: Int = 0)
+                              (implicit stanzaMap: Map[String, Stanza]): RequestOutcome[(Seq[VisualStanza], Labels, Seq[String], String, Option[DataInput])] =
     stanzaMap.get(stanzaId) match {
-      case None => (visualStanzas, labels, seen, stanzaId, None)
-      case Some(s) => s match {
-        case EndStanza => (visualStanzas, labels, seen :+ stanzaId, stanzaId, None)
-        case s: VisualStanza with DataInput => (visualStanzas :+ s, labels, seen :+ stanzaId, stanzaId, Some(s))
+      case None => Right((visualStanzas, labels, seen, stanzaId, None))
+      case Some(s) if stanzaCount < PageStanzaLimit  => s match {
+        case EndStanza => Right((visualStanzas, labels, seen :+ stanzaId, stanzaId, None))
+        case s: VisualStanza with DataInput => Right((visualStanzas :+ s, labels, seen :+ stanzaId, stanzaId, Some(s)))
         case s: Stanza with Evaluate =>
           val (next, updatedLabels) = s.eval(labels)
-          evaluateStanzas(next, updatedLabels, visualStanzas, seen :+ stanzaId)
-        case s: VisualStanza => evaluateStanzas(s.next.head, labels, visualStanzas :+ s, seen :+ stanzaId)
+          evaluateStanzas(next, updatedLabels, visualStanzas, seen :+ stanzaId, stanzaCount+1)
+        case s: VisualStanza => evaluateStanzas(s.next.head, labels, visualStanzas :+ s, seen :+ stanzaId, stanzaCount+1)
       }
+      case Some(s) => Left(NonTerminatingPageError)
     }
 }
