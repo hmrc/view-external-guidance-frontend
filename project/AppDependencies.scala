dd39fdbac7b83492753fec61f6ce9c8acaa4a09c
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2022-05-26 11:51:34 +0100

    DL-7800, dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,21 +4,21 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % "0.63.0",
-    "uk.gov.hmrc"       %% "play-language"              % "5.2.0-play-28",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % "0.64.0",
+    "uk.gov.hmrc"       %% "play-language"              % "5.3.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "5.20.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "3.15.0-play-28"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "3.20.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-28"    % "0.63.0"        % "test",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-28"    % "0.64.0"        % "test",
     "org.scalamock"                %% "scalamock"                  % "5.2.0"         % "test",
-    "org.jsoup"                    %  "jsoup"                      % "1.14.3"        % "test",
+    "org.jsoup"                    %  "jsoup"                      % "1.15.1"        % "test",
     "com.typesafe.play"            %% "play-test"                  % current         % "test",
     "org.scalatestplus.play"       %% "scalatestplus-play"         % "5.1.0"         % "test",
     "org.pegdown"                  %  "pegdown"                    % "1.6.0"         % "test, it",
     "com.github.tomakehurst"       %  "wiremock-jre8"              % "2.33.2"        % "test, it",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.13.2"        % "test, it",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.13.3"        % "test, it",
     "uk.gov.hmrc"                  %% "bootstrap-test-play-28"     % "5.20.0"        % "test, it"
   )
 }
