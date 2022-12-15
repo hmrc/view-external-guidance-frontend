db4d4f47c6e845ee97524bcb8cc97d5591e30c2d
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2022-12-15 14:16:54 +0000

    DL-9201, test updates

--- test/models/admin/AdminPackageSpec.scala
+++ test/models/admin/AdminPackageSpec.scala
@@ -17,64 +17,28 @@
 package models.admin
 
 import base.BaseSpec
+import core.models.ocelot.Phrase
+import core.models.ocelot.stanzas._
 
 class AdminPackageSpec extends BaseSpec {
 
   "stanzaTypeName" must {
 
+    "support PageStanza" in {
+      stanzaTypeName(PageStanza("/usr",Seq("1"), false)) shouldBe "Page"
+    }
+    "support Instruction" in {
+      stanzaTypeName(Instruction(Phrase("Input", "Input"), Seq("1"), None, false)) shouldBe "Instruction"
+    }
+    "support all Callout stanza types" in {
+      stanzaTypeName(ErrorCallout(Phrase("Input", "Input"), Seq("1"), false)) shouldBe "Error"
+    }
+    "support Calculation" in {
+      stanzaTypeName(Calculation(Seq("1"), Seq(AddOperation("l","op","r")))) shouldBe "Calculation"
+    }
+    "support ValueStanza" in {
+      stanzaTypeName(ValueStanza(List(Value(ScalarType, "l", "0")), Seq("1"), false)) shouldBe "Value"
+    }
   }
-
-  // def stanzaTypeName(s: PopulatedStanza): String =
-  //   s match {
-  //     case _: PageStanza => "Page"
-  //     case _: Instruction => "Instruction"
-  //     case _: TitleCallout => "Title"
-  //     case _: SubTitleCallout => "SubTitle"
-  //     case _: SectionCallout => "Section"
-  //     case _: SubSectionCallout => "SubSection"
-  //     case _: LedeCallout => "Lede"
-  //     case _: ErrorCallout => "Error"
-  //     case _: ValueErrorCallout => "ValueError"
-  //     case _: TypeErrorCallout => "TypeError"
-  //     case _: ImportantCallout => "Important"
-  //     case _: YourCallCallout => "YourCall"
-  //     case _: NumberedListItemCallout => "NumberedListItem"
-  //     case _: NumberedCircleListItemCallout => "NumberedCircleListItem"
-  //     case _: NoteCallout => "NoteCallout"
-  //     case _: Question => "Question"
-  //     case _: Sequence => "Sequence"
-  //     case _: NumberInput => "NumberInput"
-  //     case _: TextInput => "TextInput"
-  //     case _: CurrencyInput => "CurrencyInput"
-  //     case _: CurrencyPoundsOnlyInput => "CurrencyPoundsOnlyInput"
-  //     case _: DateInput => "DateInput"
-  //     case _: Calculation => "Calculation"
-  //     case _: Choice => "Choice"
-  //     case _: Row => "Row"
-  //     case _: ValueStanza => "Value"
-
-  //     case _ => "Other"
-  //   }
-
-  // def operationName(op: Operation): String =
-  //   op match {
-  //     case _: AddOperation => "Add"
-  //     case _: SubtractOperation => "Subtract"
-  //     case _: MultiplyOperation => "Multiply"
-  //     case _: DivideOperation => "Divide"
-  //     case _: CeilingOperation => "Ceiling"
-  //     case _: FloorOperation => "Floor"
-  //   }
-
-  // def choiceTestName(op: ChoiceTest): String =
-  //   op match {
-  //     case _: EqualsTest => "equals"
-  //     case _: NotEqualsTest => "not equals"
-  //     case _: MoreThanTest => "more than"
-  //     case _: MoreThanOrEqualsTest => "more than or equals"
-  //     case _: LessThanTest => "less than"
-  //     case _: LessThanOrEqualsTest => "less than or equals"
-  //     case _: ContainsTest => "contains"
-  //   }
 
 }
