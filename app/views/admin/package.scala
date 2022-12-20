6823fa88352877387d970fb8f6388340b36c8ac4
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2022-12-20 10:29:25 +0000

    DL-9201-fix

--- app/views/admin/package.scala
+++ app/views/admin/package.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package models
+package views
 
 import core.models.ocelot.stanzas._
 
--- test/views/admin/AdminPackageSpec.scala
+++ test/views/admin/AdminPackageSpec.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package models.admin
+package views.admin
 
 import base.BaseSpec
 import core.models.ocelot.Phrase
