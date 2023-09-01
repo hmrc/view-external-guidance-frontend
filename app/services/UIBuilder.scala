6783cd4b1d66c5a67449d3858f129606b31c17d6
christopher.richardson <55755517+cmrhmrc@users.noreply.github.com>
2023-09-01 12:38:21 +0100

    DL-11639, fixed and refactored handling of groups of stacked stanzas

--- app/services/UIBuilder.scala
+++ app/services/UIBuilder.scala
@@ -20,7 +20,7 @@ import javax.inject.Singleton
 import scala.util.matching.Regex._
 import models._
 import models.ocelot.stanzas._
-import core.models.errors.{Error, InternalServerError}
+import core.models.errors.Error
 import core.models.RequestOutcome
 import core.models.ocelot.stanzas.{CurrencyInput, CurrencyPoundsOnlyInput, DateInput, Input, Question}
 import core.models.ocelot.stanzas._
@@ -67,14 +67,17 @@ class UIBuilder {
   }
 
   @tailrec
-  private def fromStanzas(stanzas: List[VisualStanza], acc: List[UIComponent], errStrategy: ErrorStrategy)(implicit ctx: UIContext): RequestOutcome[List[UIComponent]] =
+  private def fromStanzas(stanzas: List[VisualStanza], acc: List[UIComponent], errStrategy: ErrorStrategy)
+                         (implicit ctx: UIContext): RequestOutcome[List[UIComponent]] =
     stanzas match {
       case Nil => Right(acc)
-      case (sg: StackedGroup) :: xs =>
-        fromStackedGroup(sg, errStrategy) match {
-          case Left(err) => Left(err)
-          case Right(uiComponents) => fromStanzas(xs, acc ++ uiComponents, errStrategy)
-        }
+      case StackedGroup(_, (c: SubSectionCallout) :: (rg: RowGroup) :: xs, _) :: others if rg.isTableCandidate =>
+        fromStanzas(stackStanzas(Nil)(xs) ++ others, acc ++ List(fromTableRowGroup(TextBuilder.fromPhrase(c.text), rg)), errStrategy)
+      case StackedGroup(_, (c: SubSectionCallout) :: (ng: NoteGroup) :: xs, _) :: others =>
+        fromStanzas(stackStanzas(Nil)(xs) ++ others, acc ++ List(fromSectionAndNoteGroup(TextBuilder.fromPhrase(c.text), ng)), errStrategy)
+      case StackedGroup(_, (c: SubSectionCallout) :: (nc: NoteCallout) :: xs, _) :: others =>
+        fromStanzas(stackStanzas(Nil)(xs) ++ others, acc ++ List(fromSectionAndNoteCallout(TextBuilder.fromPhrase(c.text), nc)), errStrategy)
+      case StackedGroup(_, x :: xs, _) :: others => fromStanzas(x :: stackStanzas(Nil)(xs) ++ others, acc, errStrategy)
       case (eg: RequiredErrorGroup) :: xs => fromStanzas(xs, acc ++ fromRequiredErrorGroup(eg, errStrategy), errStrategy)
       case (tg: TypeErrorGroup) :: xs => fromStanzas(xs, acc ++ fromTypeErrorGroup(tg, errStrategy), errStrategy)
       case (i: Instruction) :: xs => fromStanzas(xs, acc ++ List(fromInstruction(i)), errStrategy)
@@ -93,24 +96,9 @@ class UIBuilder {
       case x :: xs if ctx.labels.runMode == PageReview =>
         logger.warn(s"Encountered and ignored (PageReview usage) invalid VisualStanza due to accessibility rules, $x")
         fromStanzas(xs, acc, errStrategy)
-      case x :: xs =>
+      case x :: _ =>
         logger.warn(s"Encountered invalid VisualStanza due to accessibility rules, $x")
         Left(Error(UnsupportedUiPatternError, ctx.labels.runMode, x.next.headOption))
-    }
-
-  private def fromStackedGroup(sg: StackedGroup, errStrategy: ErrorStrategy)(implicit ctx: UIContext): RequestOutcome[Seq[UIComponent]] =
-    sg.group.toList match {
-      case (c: SubSectionCallout) :: (rg: RowGroup) :: xs if rg.isTableCandidate =>
-        fromStanzas(stackStanzas(Nil)(xs), List(fromTableRowGroup(TextBuilder.fromPhrase(c.text), rg)), errStrategy)
-      case (c: SubSectionCallout) :: (ng: NoteGroup) :: xs  =>
-        fromStanzas(stackStanzas(Nil)(xs), List(fromSectionAndNoteGroup(TextBuilder.fromPhrase(c.text), ng)), errStrategy)
-      case (c: SubSectionCallout) :: (nc: NoteCallout) :: xs  =>
-        fromStanzas(stackStanzas(Nil)(xs), List(fromSectionAndNoteCallout(TextBuilder.fromPhrase(c.text), nc)), errStrategy)
-      case x :: xs => // No recognised stacked pattern
-        fromStanzas(x +: stackStanzas(Nil)(xs), Nil, errStrategy)
-      case Nil => // No recognised stacked pattern
-        logger.error("Empty stack group found")
-        Left(InternalServerError)
     }
 
   private def fromCYASummaryListRowGroup(rg: RowGroup)(implicit ctx: UIContext): UIComponent =
