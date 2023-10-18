666f37bcacd61cbe97d1ebae8f726e6dd6e253ff
christopher.richardson <55755517+cmrhmrc@users.noreply.github.com>
2023-10-18 11:53:59 +0100

    DL-11986, add process cache access api

---
+++ app/controllers/apis/AdminApiController.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.apis
+
+import config.AppConfig
+import play.api.libs.json.{Format, Json}
+import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import repositories.ProcessCacheRepository
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+import play.api.Logging
+import models.admin.CachedProcessSummary
+
+// case class ActiveProcessSummary(id: String, processVersion: Long, title: String, expiryTime: Instant)
+// object ActiveProcessSummary {
+//   implicit lazy val formats: Format[ActiveProcessSummary] = Json.format[ActiveProcessSummary]
+// }
+
+@Singleton
+class AdminApiController @Inject() (appConfig: AppConfig, processCacheRepository: ProcessCacheRepository, mcc: MessagesControllerComponents) 
+  extends FrontendController(mcc) with Logging {
+
+  implicit val config: AppConfig = appConfig
+  implicit val ec: ExecutionContext = mcc.executionContext
+  implicit lazy val cachedProcessSummaryFormats: Format[CachedProcessSummary] = Json.format[CachedProcessSummary]
+
+  def listActiveProcessSummaries(): Action[AnyContent] = Action.async {_ =>
+    processCacheRepository.listSummaries().map{
+      case Right(list: List[CachedProcessSummary]) => 
+        logger.info(s"Returning $list")
+        Ok(Json.toJson(list))
+      case Left(err) => 
+        logger.error(s"Unable to retrieve list of active process summaries, error : $err")
+        InternalServerError("")
+    }
+  }
+
+  def getActive(id: String, version: String): Action[AnyContent] = Action.async { _ =>    
+    processCacheRepository.get(id, version.toLong).map {
+      case Right(cachedProcess) => Ok(Json.toJson(cachedProcess.process))
+      case Left(err) =>
+        logger.error(s"Unable to retrieve active process ($id, $version), err = $err")
+        BadRequest(s"Unable to retrieve active process ($id, $version), err = $err")
+    }
+  }  
+
+}
