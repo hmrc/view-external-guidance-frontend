6e84af0c1fcbfe97c0619b7d481ccc134700d397
christopher.richardson <55755517+cmrhmrc@users.noreply.github.com>
2023-10-23 11:05:47 +0100

    DL-11533, dependency & pr commenter updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,16 +7,16 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % "1.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.21.0-play-28"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.23.0-play-28"
   )
 
   val test = Seq(
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-28"    % "1.3.0"        % "test",
     "org.scalamock"                %% "scalamock"                  % "5.2.0"         % "test",
-    "org.jsoup"                    %  "jsoup"                      % "1.16.1"        % "test",
+    "org.jsoup"                    %  "jsoup"                      % "1.16.2"        % "test",
     "org.pegdown"                  %  "pegdown"                    % "1.6.0"         % "test",
     "com.github.tomakehurst"       %  "wiremock-jre8"              % "2.35.0"        % "test",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.15.2"        % "test",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.15.3"        % "test",
     "uk.gov.hmrc"                  %% "bootstrap-test-play-28"     % bootstrapVersion        % "test"
   )
 
@@ -24,7 +24,7 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% "bootstrap-test-play-28" % bootstrapVersion  % Test,
     "org.pegdown"                   %  "pegdown"                % "1.6.0"           % Test,
     "com.github.tomakehurst"        % "wiremock-jre8"           % "2.35.0"          % Test,
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"   % "2.15.2"          % Test,
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"   % "2.15.3"          % Test,
     "uk.gov.hmrc"                   %% "bootstrap-test-play-28" % bootstrapVersion  % Test
   )
 }
