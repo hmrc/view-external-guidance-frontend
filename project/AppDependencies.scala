c68e6316ad809295f4f355f679a810d50a3ae3ef
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2023-12-13 16:28:57 +0000

    DL-12280, working with HmrcStandardPage and play-frontend-hmrc 7.29.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % "1.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.23.0-play-28"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.29.0-play-28"
   )
 
   val test = Seq(
