dfae21cbc38bf738eedab3b53303ac107e70064a
rameshmandanapu1 <155470931+rameshmandanapu1@users.noreply.github.com>
2024-02-26 16:13:28 +0000

    Removed unsused dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,17 +13,10 @@ object AppDependencies {
   val test = Seq(
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-30"    % "1.7.0"        % "test",
     "org.scalamock"                %% "scalamock"                  % "5.2.0"        % "test",
-    "org.jsoup"                    %  "jsoup"                      % "1.17.2"       % "test",
-    "org.pegdown"                  %  "pegdown"                    % "1.6.0"        % "test",
-    "com.github.tomakehurst"       %  "wiremock-jre8"              % "3.0.1"        % "test",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.16.1"       % "test",
     "uk.gov.hmrc"                  %% "bootstrap-test-play-30"     % bootstrapVersion % "test",
   )
 
   val itDependencies = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-test-play-30" % bootstrapVersion  % Test,
-    "org.pegdown"                   %  "pegdown"                % "1.6.0"           % Test,
-    "com.github.tomakehurst"        % "wiremock-jre8"           % "3.0.1"           % Test,
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"   % "2.16.1"          % Test,
   )
 }
