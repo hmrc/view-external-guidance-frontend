8a4d649da97c2228711c719bb571d6eebd68b31a
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2024-06-10 15:05:26 +0100

    DL-10091, fixed conflict

--- app/core/services/Rates.scala
+++ app/core/services/Rates.scala
@@ -21,7 +21,7 @@ import core.models.ocelot._
 import scala.util.matching.Regex
 
 @Singleton
-class Rates @Inject() () extends LabelledDataExpansion with LabelledDataReferencing{<<<<<<< HEAD
+class Rates @Inject() () extends LabelledDataExpansion with LabelledDataReferencing {
   val KeySeparator: String = "!"
   private val SectionId: Int = 1
   private val RateId: Int = 2
